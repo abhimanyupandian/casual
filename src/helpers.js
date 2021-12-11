7087732b4f0c26d1ba7c77c87b0d38e2bc4b43e9
Abhimanyu Pandian <36107381+abhimanyupandian@users.noreply.github.com>
2021-12-11 19:04:42 +0530

    Initial commit

--- src/helpers.js
+++ src/helpers.js
@@ -25,6 +25,28 @@ var random_value = function(object) {
 	return object[this.random_key(object)];
 };
 
+var random_string = function({min_length=0, max_length=1, extras = [], exclude_digits=false, exclude_letters=false}) {
+	var casual = this;
+	if (!(extras instanceof Array)){
+		throw new TypeError('Argument "extras" is not an Array');
+	}
+	if (!(Number.isInteger(min_length) && min_length >= 0)){
+		throw new TypeError('Argument "min" is not an Integer');
+	}
+	if (!(Number.isInteger(max_length) && max_length >= 0)){
+		throw new TypeError('Argument "max" is not an Integer');
+	}
+	if (!(typeof exclude_digits == "boolean")) {
+		throw new TypeError('Argument "exclude_digits" is not an true/false');
+	}
+	if (!(typeof exclude_letters == "boolean")) {
+		throw new TypeError('Argument "exclude_letters" is not true/false');
+	}
+
+	_length = casual.integer(from = min_length, to = max_length)
+	return this.randify({format: "#".repeat(_length), extras: extras, exclude_digits: exclude_digits, exclude_letters: exclude_letters}) 
+};
+
 var register_provider = function(provider) {
 	for (var i in provider) {
 		this.define(i, provider[i]);
@@ -65,6 +87,34 @@ var letterify = function(format) {
 	return format.replace(/X/g, this._letter);
 };
 
+var randify = function({format="##-##-##", extras = [], exclude_digits=false, exclude_letters=false}) {	
+	var casual = this;
+	if (!(extras instanceof Array)){
+		throw new TypeError('Argument "extras" is not an Array');
+	} if (!extras.every(function (e) { return e.toString().length == 1;})){
+		throw new TypeError('Argument "extras" is not an Array containing only chars. ie; strings with length 1');
+	}
+
+	_bucket = []
+	if (extras.length) {
+		_bucket.push(function() {return casual.random_element(extras)})
+	}
+	if (!(exclude_digits)) {
+		_bucket.push(casual._digit)
+	}
+	if (!(exclude_letters)) {
+		_bucket.push(casual._letter)
+	}
+	
+	if (!(_bucket.length)){
+		return false
+	}
+
+	return format.replace(/#/g, function() {
+		return casual.random_element(_bucket)();
+	});
+};
+
 var join = function() {
 	var tokens = Array.prototype.slice.apply(arguments);
 	return tokens.filter(Boolean).join(' ');
@@ -93,5 +143,7 @@ module.exports = {
 	letterify:letterify,
 	join: join,
 	populate: populate,
-	populate_one_of: populate_one_of
+	populate_one_of: populate_one_of,
+	randify: randify,
+	random_string: random_string
 };
